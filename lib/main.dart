28b506e0b664e98691e14087f10f902b803026dd
Lucas Reis <lucasreis_9@hotmail.com>
2023-05-11 15:28:14 -0300

    AudioPlayer from Local Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,20 +16,69 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home('Flutter Demo Home Page'),
+      home: AudioPlayerScreen(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  Home(this._text, {super.key});
+class AudioPlayerScreen extends StatefulWidget {
+  const AudioPlayerScreen({super.key});
 
-  String _text;
+  @override
+  State<AudioPlayerScreen> createState() => _AudioPlayerScreenState();
+}
+
+class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  late AudioPlayer _audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer()..setAsset('assets/audio/Foco1.mp3');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _audioPlayer.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text(_text),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.keyboard_arrow_down_rounded),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_horiz),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0XFF144771),
+              Color(0XFF071A2C),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
       ),
     );
   }
